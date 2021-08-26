a05ec4f2f21eab517ac91a6b116a2ac4654d5879
phoomtyson <88759272+phoomtyson@users.noreply.github.com>
2021-08-27 00:14:55 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    W = float(input("Input base : "))
+    H = float(input("Input height : "))
+    A = W * H / 2
+    print("Triangle area : " + str(A))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
